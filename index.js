44d7cbc74303bde4f87a1824145c7e826a9f92da
jikkuvijay2000 <jikkuvijay21@gmail.com>
2024-05-23 13:44:59 +0530

    changes

--- index.js
+++ index.js
@@ -9,9 +9,26 @@ const { todoRouter } = require('./Router/todo.js');
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(cors(
-  { origin : "https://bright-dusk-f55498.netlify.app/"}
-))
+const allowedOrigins = [
+    'https://bright-dusk-f55498.netlify.app'
+    // Add more origins if necessary
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        // Check if the origin is in the allowed origins list
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true // If you need to send cookies or authorization headers
+};
+
+// Use the CORS middleware
+app.use(cors(corsOptions));
+
 
 app.use("/api",userRouter);
 app.use("/todo",todoRouter);
